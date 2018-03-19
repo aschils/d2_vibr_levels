a09babf087b783ebe8a353d2659f902b720e55b8
Arnaud Schils <arnaud.schils@gmail.com>
2018-03-19 18:36:54 +0100

    He2

--- numerov.py
+++ numerov.py
@@ -23,14 +23,15 @@ import matplotlib.pyplot as plt
 h = 6.626*10**-34 #Planck Constant [J s]
 hbar = h/(2*math.pi)
 m_p = 1.672*10**-27 #Proton mass [kg]
-m = 4*m_p #approximation of D_2 mass [kg]
+mr_d2 = 4*m_p #approximation of D_2 mass [kg]
+mr_he2 = 2*m_p
 
 electron_charge = 1.60217662*10**-19 #coulomb
 bohr_to_meter = 5.291772*10**-11
 
 # <psi1 | psi2> = int_{-\infty}^{\infty} psi1(r)^* psi2(r) dr
 def wave_fun_scalar_prod(psi1, psi2, dr):
-    return np.dot(psi1,psi2)*dr
+    return np.dot(psi1,psi2)*dr-dr/2.0*psi1[0]*psi2[0]-dr/2.0*psi1[-1]*psi2[-1]
 
 def normalize_continuum_wave_fun(psi, energy, r):
     mu = m_p #masse raduite = masse deuterium/2 = mp
@@ -81,23 +82,27 @@ def wave_fun_scalar_prod_from_fun(psi1, psi2, r_0, r_e):
 # eigen_vectors: 2D numpry array, eigen_vectors[i] is the Psi_i
 #   eigen_vectors[i][j] is Psi_i(r_j), r_j are the values in t_bohr
 #
-def numerov(pot_file_path, is_bound_potential, E_max,
+def numerov(pot_file_path, is_bound_potential, E_max, m,
 r_end_for_unbound_potential=10, refine=1):
 
     pot_file = np.loadtxt(pot_file_path)
 
     r = pot_file[:,0]*bohr_to_meter
-    V = pot_file[:,1]*electron_charge #Joule
-    V = interp1d(r,V,kind="linear",fill_value="extrapolate")
-
+    V_discr = pot_file[:,1]*electron_charge #Joule
+    V = interp1d(r,V_discr,kind="linear",fill_value="extrapolate")
+    #plt.plot(r,V(r))
+    #plt.show()
     r_inf = 10**6 #purely arbitrary
 
     if is_bound_potential:
-        min_V_result = sp.optimize.minimize_scalar(V)
-        if not min_V_result.success:
-            raise Exception("Minimum of potential V not found.")
-        r_min_of_V = min_V_result.x
-        E_min = V(r_min_of_V)
+        #min_V_result = sp.optimize.minimize_scalar(V)
+        #if not min_V_result.success:
+        #    raise Exception("Minimum of potential V not found.")
+        #r_min_of_V = min_V_result.x
+        #E_min = V(r_min_of_V)
+        i_min = np.argmin(V_discr)
+        E_min = V_discr[i_min]
+        r_min_of_V = r[i_min]
     else:
         E_min = V(r_inf)
         r_min_of_V = r_inf
@@ -207,25 +212,30 @@ r_bohr_free, E):
     p_E = 0
     dr_bound = r_bohr_bound[1]-r_bohr_bound[0]
 
+    #Find dissociated state from the inter-atomic distance in the
+    #bound state
+    final_free_state = final_dissoc_state(eigen_values_free,
+    eigen_vectors_free, E)
+    final_free_statef = interp1d(r_bohr_free, final_free_state,
+    kind=0,fill_value="extrapolate")
+    final_free_state = final_free_statef(r_bohr_bound)
+
     for ev_b_idx in range(0, eigen_values_bound.size):
 
         e_val_b = eigen_values_bound[ev_b_idx]
         e_vec_b = eigen_vectors_bound[ev_b_idx]
+
         #Proba to be in vibrational bound state v
         proba_v = bound_vib_level_distrib(e_val_b)
+
         #proba_inter_atomic_dist = e_vec_b(r_bohr)**2
 
-        #Find dissociated state from the inter-atomic distance in the
-        #bound state
-        final_free_state = final_dissoc_state(eigen_values_free,
-        eigen_vectors_free, E)
+        #plt.plot()
 
         #Transition probability = scalar product between initial and
         #final states
 
-        final_free_statef = interp1d(r_bohr_free, final_free_state,
-        kind=0,fill_value="extrapolate")
-        final_free_state = final_free_statef(r_bohr_bound)
+
         trans_proba = wave_fun_scalar_prod(e_vec_b, final_free_state, dr_bound)**2
 
         #trans_proba = wave_fun_scalar_prod_from_fun(final_free_state, e_vec_b,
@@ -255,53 +265,110 @@ r_bohr_free, E):
 
 #E_min 10.8198528842
 
-(r_bohr_bound, V_bound, eigen_values_bound, eigen_vectors_bound) = numerov("pot_d2+.txt", True, 13, refine=3)
-(r_bohr_free, V_free, eigen_values_free, eigen_vectors_free) = numerov("pot_d2_b.txt", False, 10, refine=1)
+#(r_bohr_bound, V_bound, eigen_values_bound, eigen_vectors_bound) = numerov("pot_d2+.txt", True, 13, refine=3)
+#(r_bohr_free, V_free, eigen_values_free, eigen_vectors_free) = numerov("pot_d2_b.txt", False, 10, refine=1)
 
 def bound_vib_level_distrib(mol_energy):
     return 1/eigen_values_bound.size
 
-
-def plot_wave_fun(eigen_vectors, eigen_values, r_bohr, V):
+def under_plot_wave_fun(eigen_vectors, eigen_values, r_bohr, V, plot_ratio=1):
 
     r = r_bohr*bohr_to_meter
-    plt.plot(r_bohr, V(r)/electron_charge)
+    plt.plot(r_bohr, V(r))
 
-    for i in range(0, 100):
-        if i%10 == 0:
+    for i in range(0, len(eigen_vectors)):
+        if i%plot_ratio == 0:
+        #if True:
             psi = eigen_vectors[i]
             #print(eigen_values[i])
             #print(psi)
 
-            plt.plot(r_bohr, psi**2/np.linalg.norm(psi**2)+eigen_values[i])
-            #plt.plot(r_bohr, psi/np.linalg.norm(psi)+eigen_values[i])
+            #plt.plot(r_bohr, psi**2/np.linalg.norm(psi**2)+eigen_values[i])
+            plt.plot(r_bohr, psi/np.linalg.norm(psi)+eigen_values[i])
         #print(energies[i])
+    #plt.show()
+
+def plot_wave_fun(eigen_vectors, eigen_values, r_bohr, V, plot_ratio=1):
+    V_ev = lambda r: V(r)/electron_charge
+    under_plot_wave_fun(eigen_vectors, eigen_values, r_bohr, V_ev, plot_ratio=1)
     plt.show()
 
-plot_wave_fun(eigen_vectors_bound, eigen_values_bound, r_bohr_bound, V_bound)
-plot_wave_fun(eigen_vectors_free, eigen_values_free, r_bohr_free, V_free)
+def plot_bound_and_free(eigen_vectors_bound, eigen_values_bound, r_bohr_bound, V_bound,
+eigen_vectors_free, eigen_values_free, r_bohr_free, V_free, plot_ratio_bound=1, plot_ratio_free=1,
+    V_free_shift=0):
+    V_bound_ev = lambda r: V_bound(r)/electron_charge
+    under_plot_wave_fun(eigen_vectors_bound, eigen_values_bound, r_bohr_bound, V_bound_ev, plot_ratio_bound)
+    V_free_shifted = lambda r: V_free(r)/electron_charge + V_free_shift
+    under_plot_wave_fun(eigen_vectors_free, eigen_values_free+V_free_shift, r_bohr_free, V_free_shifted, plot_ratio_free)
+    plt.show()
 
-proba_e = []
-energies = np.linspace(0, 10, 2000)
-delta_e = energies[1]-energies[0]
-I = 0
-i = 0
-delta_disp = energies.size/100
-disp_count = delta_disp
-for e in energies:
-    p_e = ker(bound_vib_level_distrib, eigen_values_bound,
-    eigen_vectors_bound, eigen_values_free, eigen_vectors_free, r_bohr_bound,
-    r_bohr_free, e)
-    proba_e.append(p_e)
-    I = I+p_e*delta_e
+(r_bohr_free, V_free, eigen_values_free, eigen_vectors_free) = numerov("pot_rag_free.txt", False, 3, mr_he2, refine=3)
+(r_bohr_bound, V_bound, eigen_values_bound, eigen_vectors_bound) = numerov("pot_rag.txt", True, 2.3, mr_he2, refine=7)
 
-    progress = i/energies.size*100
-    if i / disp_count == 10:
-        print("Progress  "+str(progress)+"%")
-        disp_count = disp_count+delta_disp
-    i = i+1
+plot_bound_and_free(eigen_vectors_bound, eigen_values_bound, r_bohr_bound, V_bound,
+eigen_vectors_free, eigen_values_free, r_bohr_free, V_free,3,10,0.0899*27.2)
 
-print(I)
+#eigen_values_free_cm = eigen_values_free*8065.54429
 
-plt.plot(energies, proba_e)
-plt.show()
+# article_cm = np.array([839.53,
+# 2465.81,
+# 4021.51,
+# 5506.77,
+# 6920.80,
+# 8264.30,
+# 9538.59,
+# 10744.29,
+# 11881.04,
+# 12947.69,
+# 13942.75,
+# 14864.74,
+# 15712.07,
+# 16483.21,
+# 17176.57,
+# 17790.42,
+# 18322.96
+# #18772.77
+# #19138.51,
+# #19422.68,
+# #19626.76,
+# #19729.67
+# ])
+#
+# diff_with_art = article_cm-eigen_values_free_cm
+# print(diff_with_art/article_cm)
+#
+# print(article_cm)
+# print(eigen_values_free_cm)
+
+#
+# plot_wave_fun(eigen_vectors_bound, eigen_values_bound, r_bohr_bound, V_bound)
+# plot_wave_fun(eigen_vectors_free, eigen_values_free, r_bohr_free, V_free)
+#
+# proba_e = []
+# energies = np.linspace(0, 10, 2000)
+# delta_e = energies[1]-energies[0]
+# I = 0
+# i = 0
+# delta_disp = energies.size/100
+# disp_count = delta_disp
+# for e in energies:
+#     p_e = ker(bound_vib_level_distrib, eigen_values_bound,
+#     eigen_vectors_bound, eigen_values_free, eigen_vectors_free, r_bohr_bound,
+#     r_bohr_free, e)
+#     proba_e.append(p_e)
+#     I = I+p_e*delta_e
+#
+#     progress = i/energies.size*100
+#     if i / disp_count == 10:
+#         print("Progress  "+str(progress)+"%")
+#         disp_count = disp_count+delta_disp
+#     i = i+1
+#
+# print(I)
+#
+# plt.plot(energies, proba_e)
+# plt.show()
+
+#f1 = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1,1,1])
+#f2 = np.array([0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5])
+#print(wave_fun_scalar_prod(f1, f2, 0.5))
